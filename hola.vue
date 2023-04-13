fb8e689718dafbb00fe8e235a85009b614a4fbf7
Mateo Asus <m2a3c11@gmail.com>
2023-04-13 15:17:16 -0500

    transitions

---
+++ hola.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>Hola para la prueba</p>
+        <div>{{ text }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                text: "Hello world",
+            };
+        },
+    };
+</script>
+
+<style>
+p {
+    color: red;
+}
+</style>
---
+++ options/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="show = !show">Menú</button>
+  <MenuPrueba v-if="show" />
+</template>
+
+<script>
+import MenuPrueba from "./components/MenuPrueba.vue";
+export default {
+  name: "App",
+  components: { MenuPrueba },
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
