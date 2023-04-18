be2a97424a3a489ea795bdaff206bc63028b5ff7
Mateo Asus <m2a3c11@gmail.com>
2023-04-18 11:39:33 -0500

    Hooks

--- options/src/App.vue
+++ options/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <div>
-    <ModalPrueba />
-  </div>
+  <div>{{ text }}</div>
 </template>
 
 <script>
-import ModalPrueba from "./components/ModalPrueba.vue";
 export default {
   name: "App",
-  components: {
-    ModalPrueba,
+  data() {
+    return {
+      text: "Hola Vue",
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate", this.$data, this.$el);
+  },
+  created() {
+    console.log("created", this.$data, this.$el);
+  },
+  mounted() {
+    console.log("mounted", this.$data, this.$el);
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
