4ed396a7032b50c1ef50c0d17315e12ca0b8ff72
Mateo Asus <m2a3c11@gmail.com>
2023-04-18 12:26:28 -0500

    Ciclo de vida compositiosn API

--- options/src/App.vue
+++ options/src/App.vue
@@ -1,27 +1,14 @@
 <template>
-  <div>{{ text }}</div>
-  <div>{{ algo }}</div>
+  <HomeSetup></HomeSetup>
 </template>
 
 <script>
-import base from "@/mixins/base";
+import HomeSetup from "./components/HomeSetup.vue";
 
 export default {
   name: "App",
-  mixins: [base],
-  data() {
-    return {
-      text: "Hola Vue",
-    };
-  },
-  beforeCreate() {
-    console.log("beforeCreate", this.$data, this.$el);
-  },
-  created() {
-    console.log("created", this.$data, this.$el);
-  },
-  mounted() {
-    console.log("mounted", this.$data, this.$el);
+  components: {
+    HomeSetup,
   },
 };
 </script>
